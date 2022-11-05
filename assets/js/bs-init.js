725a5eb108cc747427baf82e0c4d204537f0a3dd
tusoporteapp <tusoporte.app@gmail.com>
2022-11-05 12:32:46 -0500

    update

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -10,4 +10,12 @@ if (window.innerWidth < 768) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+
+	var products = document.querySelectorAll('[data-bss-dynamic-product]');
+
+	for (var product of products) {
+		var param = product.dataset.bssDynamicProductParam;
+		product.dataset.reflowProduct = new URL(location.href).searchParams.get(param)
+	}
+
 }, false);
